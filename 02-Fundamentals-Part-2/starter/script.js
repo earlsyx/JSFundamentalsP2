19ca20fd3a70ae2c23cd8f771e1ffe30fe68a0c7
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-25 05:24:08 +0800

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,42 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
-if (passTest) hasDriverLicense = true;
+// if (passTest) hasDriverLicense = true;
 if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
+
+//Function
+// - piece of code that we can reuse over and over again in our code.
+
+function logger() {
+  //function body
+  console.log("My name is Jonas");
+}
+
+logger(); //invoking, running, calling the function, executing
+logger();
+logger();
+
+//receive data and return data back/ machines, food processor
+
+//parameters defined once the function is called
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+} //parameters
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//not all function need to return somehing an not all need to have parameter or accept parameters
+// block of code you want to reuse over and over again
+
+*/
