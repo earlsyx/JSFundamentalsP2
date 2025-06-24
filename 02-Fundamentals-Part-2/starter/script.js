9e8816e00029796872e35f8a6137cc05dc063491
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-25 06:24:31 +0800

    Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -94,7 +94,7 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3));
 
-*/
+
 
 const calcAge = function (birthYear) {
   return 2037 - birthYear;
@@ -124,3 +124,59 @@ console.log(yearsUntilRetirement(1950, "Nash"));
 // return statement - to output a value from the function and terminate executiton
 // calling cfunciton rniing onviking using ()
 // arguments - acutal values of functino paramers to input data, age value variable to save return v alue function output
+
+*/
+
+// FIRST data structures ARRAY
+const friend1 = "Micheal";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+//Arrays and objeccts importantant
+const friends = ["Micheal", "Steven", "Peter"]; // literral syntax
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]); //arrays 0 based
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); //expression not stataement
+
+//change or mutate
+
+friends[2] = "Jay";
+console.log(friends);
+//const can't be change, but only primitive values can't.
+// arrays aren't primitive valuel ike int, string etc.
+// cause of how js stores things in memory
+
+//can't replace entire array.
+// friends = ['Bob','Alice']
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schemdtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years1 = [1990, 1967, 2002, 2010, 2018];
+
+// console.log(calcAge(years1)); //Not a Number NaN
+const age1 = calcAge(years1[0]);
+const age2 = calcAge(years1[1]);
+const age3 = calcAge(years1[years1.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years1[0]),
+  calcAge(years1[1]),
+  calcAge(years1[years1.length - 1]),
+];
+console.log(ages);
