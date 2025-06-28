d29bffb7cc71888fa6c37783ac599b19041d7d07
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-28 22:45:18 +0800

    Array - contains duplicate nad binary search

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -373,3 +373,52 @@ while (l < r) {
 return - 1;
 
 */
+
+/*Contains Duplicate HashSet*/
+
+/*
+Public class Solution {
+
+  public bool ContainsDuplicate(int[] nums) {
+     HashSet<int> seen = new HashSet<int>();
+     
+    foreach (int num in nums) {
+    if (seen.Contains(num)) {
+        return true;
+    }
+    else {
+    seen.Add(num);
+    }
+
+    }
+
+  }
+
+   return -1;
+}
+
+*/
+
+/**
+ public bool BinarySearch(int[] nums, int target) {
+  int l = 0, r = nums.Length - 1; 
+
+  while (l < r) {
+  int mid = l + (l - r) / 2
+  
+  if (target < nums[mid]) {
+     l = mid + 1;
+  }
+
+  else if (target > nums[mid])
+    r = mid - 1;
+  }
+  
+  else  {
+  return mid;
+  }
+ 
+  return -1; 
+ }
+  
+ */
